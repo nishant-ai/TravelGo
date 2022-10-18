c30848c6b9430cc81326ff3b1f034e444ae71501
sauravraw <saurav.upadhyay@contentstack.com>
2022-10-18 17:40:30 +0530

    fixed redirecting to login page

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,96 +1,94 @@
-import React from 'react'
-import styled from 'styled-components'
-import PackageCard from './PackageCard'
-import PackageSelect from './PackageSelect'
+import React from "react";
+import styled from "styled-components";
+import PackageCard from "./PackageCard";
+import PackageSelect from "./PackageSelect";
 
 function Packages() {
   return (
-    <Container  id='packages'>
-
-        <Title>Best Packages For You</Title>
-        <Package>
-            <PackageSelect title='Hot Deals' active={true} />
-            <PackageSelect title='Backpack' active={false} />
-            <PackageSelect title ='South Asia'active={false} />
-            <PackageSelect title='United States' active={false} />
-            <PackageSelect title='Russia' active={false} />
-            <PackageSelect title='More' active={false} />
-        </Package>
-        <PackageCards>
-            <PackageCard
-                location='Indonesia'
-                cost='$500 / Person'
-                tagline='Explore the Beauty of the island for 3 days and 2 nights with our travel agency'
-                stay='3 Days, 2 Nights'
-                image='indonesia2.png'
-            />
-            <PackageCard
-                location='Japan'
-                cost='$800 / Person'
-                tagline='Enjoy the Shrimes and blossoms here in this beautiful country'
-                stay='3 Days, 2 Nights'
-                image='japan.png'
-            />
-            <PackageCard
-                location='Mountains'
-                cost='$600 / Person'
-                tagline='Explore the majestic mountains and landscapes day and nights'
-                stay='3 Days, 2 Nights'
-                image='mountains.png'
-            />
-        </PackageCards>
-        <Discover href='/'>Discover More</Discover>
-
+    <Container id="packages">
+      <Title>Best Packages For You</Title>
+      <Package>
+        <PackageSelect title="Hot Deals" active={true} />
+        <PackageSelect title="Backpack" active={false} />
+        <PackageSelect title="South Asia" active={false} />
+        <PackageSelect title="United States" active={false} />
+        <PackageSelect title="Russia" active={false} />
+        <PackageSelect title="More" active={false} />
+      </Package>
+      <PackageCards>
+        <PackageCard
+          location="Indonesia"
+          cost="$500 / Person"
+          tagline="Explore the Beauty of the island for 3 days and 2 nights with our travel agency"
+          stay="3 Days, 2 Nights"
+          image="indonesia2.png"
+        />
+        <PackageCard
+          location="Japan"
+          cost="$800 / Person"
+          tagline="Enjoy the Shrimes and blossoms here in this beautiful country"
+          stay="3 Days, 2 Nights"
+          image="japan.png"
+        />
+        <PackageCard
+          location="Mountains"
+          cost="$600 / Person"
+          tagline="Explore the majestic mountains and landscapes day and nights"
+          stay="3 Days, 2 Nights"
+          image="mountains.png"
+        />
+      </PackageCards>
+      <Discover href="/">Discover More</Discover>
     </Container>
-  )
+  );
 }
 
-export default Packages
+export default Packages;
 
 const Container = styled.div`
-    height: fit-content;
-    width: 100vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    background: #ECF1F0;
-    margin-top: 5vh;
-    @media (min-width: 768px) {
-        height: 100vh;
-    }
-`
+  height: fit-content;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background: #ecf1f0;
+  margin-top: 5vh;
+  @media (min-width: 768px) {
+    height: 100vh;
+  }
+`;
 
 const Title = styled.div`
-    font-weight: 700;
-    font-size: 4vh;
-    width: 100%;
-    text-align: center;
-    margin-top: 1vh;
-`
+  font-weight: 700;
+  font-size: 4vh;
+  width: 100%;
+  text-align: center;
+  margin-top: 1vh;
+`;
 
 const Package = styled.div`
-    width: 100vw;
-    max-width: 1024px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 1vw 7vw;
-`
+  width: 100vw;
+  max-width: 1024px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1vw 7vw;
+`;
 
 const PackageCards = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    @media (min-width: 768px) {
-        flex-direction: row
-    }
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
 
 const Discover = styled.a`
   margin-top: 4vh;
-  background-color: #00A651;
+  background-color: #00a651;
   padding: 15px 20px;
   border-radius: 7px;
   font-size: 2.5vh;
@@ -99,8 +97,7 @@ const Discover = styled.a`
   cursor: pointer;
   margin-bottom: 5vh;
   text-decoration: none;
-  &:hover
-  {
+  &:hover {
     color: white;
   }
-`
+`;
